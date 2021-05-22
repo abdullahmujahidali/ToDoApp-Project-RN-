aa649752cdc62ba20966fe5dcbbe1d9c70b19f55
Abdullah Mujahid <abdullahmujahidali1@gmail.com>
2021-05-22 17:37:46 +0500

    Colors defined

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import colors from './Colors'
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+  render(){
+     return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    {/* <Image source={require('./assets/todoLogo.png')} /> */}
+      <View style={{flexDirection: "row"}}>
+        <View style={styles.divider}/>
+        <Text style={styles.title}>
+        Todo <Text style={{fontWeight:"300", color: colors.primary}}>
+          Lists
+        </Text>
+        </Text>
+        <View style={styles.divider}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
+  }
+ 
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +31,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imgStyle:{
+    height: 50,
+    width:50,
+  },
+  divider:{
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title:{
+    fontSize:38,
+    fontWeight: "800",
+    color: colors.dark,
+    paddingHorizontal:64
+  }
 });
---
+++ Colors.jsx
@@ -0,0 +1,6 @@
+export default colors ={
+    dark: "#2D3436",
+    primary: "#24A6D9",
+    lightBlue: "#A7CBD9",
+    "light": "#FFFFFF"
+}
